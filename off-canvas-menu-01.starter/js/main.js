3700988db7a67e8834fcee44f150979952e9befd
Paras Mahajan <prsmahajan@gmail.com>
2023-07-20 14:58:31 +0530

    commit

---
+++ off-canvas-menu-01.starter/js/main.js
@@ -0,0 +1,7 @@
+// Start writing JavaScript here!
+const button = document.querySelector('button')
+const body = document.body
+    
+button.addEventListener('click', e => {
+    body.classList.toggle('offsite-is-open')
+})
